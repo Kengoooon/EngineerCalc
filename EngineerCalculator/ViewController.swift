f3235b3aee6a8d90d73e1177ecb3decfd815d249
藤本健悟 <kengoooon@Macbook-Air.local>
2016-12-18 21:40:40 +0900

    Initial Commit

---
+++ EngineerCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EngineerCalculator
+//
+//  Created by 藤本健悟 on 2016/12/18.
+//  Copyright © 2016年 kengo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
